b59a4217d0278bb2c7e6a6eaa2694697b5cc834a
heesuchoi1 <heesu7563@gmail.com>
2014-10-28 17:41:18 +0900

    ff

--- sketch.js
+++ sketch.js
@@ -9,14 +9,15 @@ function setup(){
 
 function draw(){
 
-	var time = (new Date()%3000)/3000;
-	var time2 = (new Date()%100)/100;
+	//var time = (new Date()%3000)/3000;
+	//var time2 = (new Date()%100)/100;
 
 	background(bg);
 	noStroke();
-	fill(255,255,0,Math.abs(255-time2*255*2));
-	ellipse(210,300,100,Math.abs(50-time*100));
-	anime(frame);
+}
+	//fill(255,255,0,Math.abs(255-time2*255*2));
+	//ellipse(210,300,100,Math.abs(50-time*100));
+	/*anime(frame);
 	frame=frame+0.1;
 	if(frame >2){
 		frame = 0;
