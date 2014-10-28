90e0651c6bd79b6c89c2ea0dfa334e04960d2444
heesuchoi1 <heesu7563@gmail.com>
2014-10-28 17:52:37 +0900

    d

--- sketch.js
+++ sketch.js
@@ -15,33 +15,4 @@ function draw(){
 	background(bg);
 	noStroke();
 }
-	//fill(255,255,0,Math.abs(255-time2*255*2));
-	//ellipse(210,300,100,Math.abs(50-time*100));
-	/*anime(frame);
-	frame=frame+0.1;
-	if(frame >2){
-		frame = 0;
-	}
-}
-function anime(f){
-	if(f <1){
-		frame1();
-	}else if(f<2){
-		frame2();
-	}else{
-
-	}
-}
-
-
-function frame1(){
-	fill(255);
-	ellipse(10,10,100,100);
-}
-
-function frame2(){
-	fill(255,155,333);
-	ellipse(100,10,100,100);
-}
-
-*/
+	
