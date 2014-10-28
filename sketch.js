593fe77d51ded7d17861225cd8b7e89f57f42fe8
heesuchoi1 <heesu7563@gmail.com>
2014-10-28 17:59:22 +0900

    dse

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ var frame;
 function setup(){
 	bg = loadImage("BG.png");
 	createCanvas(420,600);
+	noCursor();
 	frame = 0;
 }
 
@@ -11,8 +12,11 @@ function draw(){
 
 	//var time = (new Date()%3000)/3000;
 	//var time2 = (new Date()%100)/100;
-
+	drawTest(mouseX,mouseY, 100, 100);
 	background(bg);
 	noStroke();
 }
-	
+
+function drawTest(){
+	loadImage("test.png")
+}
