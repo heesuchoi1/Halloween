c7b1064f442c08bee0de9f254a65946622c3a360
heesuchoi1 <heesu7563@gmail.com>
2014-10-28 18:00:50 +0900

    d

--- sketch.js
+++ sketch.js
@@ -12,11 +12,11 @@ function draw(){
 
 	//var time = (new Date()%3000)/3000;
 	//var time2 = (new Date()%100)/100;
-	drawTest(mouseX,mouseY, 100, 100);
+	drawTest(mouseX,mouseY,100,100);
 	background(bg);
 	noStroke();
 }
 
-function drawTest(){
+function drawTest(x,y,w,h){
 	loadImage("test.png")
 }
