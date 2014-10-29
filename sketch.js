ef7406415cae4c8e57deb1df39779e54109823d3
heesuchoi1 <heesu7563@gmail.com>
2014-10-29 16:58:16 +0900

    e

--- sketch.js
+++ sketch.js
@@ -163,21 +163,21 @@ rect(mouseX+100,mouseY+120,10,5);
 function drawSpider(){
   fill(0);
 
-  triangle(310+35,230*time,320+35,225*time,315+35,230*time);
-  triangle(310+35,230*time,315+35,230*time,325+35,240*time);
-  triangle(305+35,235*time,310+35,240*time,325+35,240*time);
-  triangle(305+35,235*time,310+35,240*time,315+35,255*time);
-  triangle(325+35,245*time,305+35,255*time,310+35,255*time);
-  triangle(320+35,265*time,305+35,255*time,310+35,255*time);
-  triangle(335+35,225*time,345+35,230*time,340+35,230*time);
-  triangle(330+35,240*time,345+35,230*time,340+35,230*time);
-  triangle(330+35,240*time,350+35,235*time,345+35,240*time);
-  triangle(340+35,255*time,350+35,235*time,345+35,240*time);
-  triangle(330+35,245*time,350+35,255*time,345+35,255*time);
-  triangle(335+35,265*time,350+35,255*time,345+35,255*time);
+  triangle(310+37,230*time,320+37,225*time,315+37,230*time);
+  triangle(310+37,230*time,315+37,230*time,325+37,240*time);
+  triangle(305+37,235*time,310+37,240*time,325+37,240*time);
+  triangle(305+37,235*time,310+37,240*time,315+37,255*time);
+  triangle(325+37,245*time,305+37,255*time,310+37,255*time);
+  triangle(320+37,265*time,305+37,255*time,310+37,255*time);
+  triangle(335+37,225*time,345+37,230*time,340+37,230*time);
+  triangle(330+37,240*time,345+37,230*time,340+37,230*time);
+  triangle(330+37,240*time,350+37,235*time,345+37,240*time);
+  triangle(340+37,255*time,350+37,235*time,345+37,240*time);
+  triangle(330+37,245*time,350+37,255*time,345+37,255*time);
+  triangle(335+37,265*time,350+37,255*time,345+37,255*time);
 
-  ellipse(327+35,250*time,15,20);
-  ellipse(327+35,237*time,5,5);
+  ellipse(327+37,250*time,15,20);
+  ellipse(327+37,237*time,5,5);
 }
 //따라다니는 별
 
