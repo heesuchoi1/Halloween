41d96ef9c29fd20601cf6bd8a3c3f1ed25f49960
heesuchoi1 <heesu7563@gmail.com>
2014-10-29 16:07:20 +0900

    erterterterte56yem ut

--- sketch.js
+++ sketch.js
@@ -281,7 +281,7 @@ ParticleSystem.prototype.run = function() {
 function mousePressed() {
   // Check if mouse is inside the circle
   var d = dist(mouseX, mouseY, 360, 200);
-  if (d < 150) {
+  if (d < 300) {
     // Pick new random color values
     r = random(255);
     g = random(255);
