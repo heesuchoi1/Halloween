1942e2f617f3f730c29ddc450545db026bf0d392
heesuchoi1 <heesu7563@gmail.com>
2014-10-29 13:03:33 +0900

    d

--- sketch.js
+++ sketch.js
@@ -16,11 +16,39 @@ function draw(){
 	noStroke();
 
 	fill(Math.random()*255,Math.random()*255,Math.random()*255,100);
-     var y;
-     y=300;
+    var y;
+    y=300;
      
-     ellipse(20,180,10,10);
-     ellipse(120,80,15,15);
+    ellipse(20,180,10,10);
+    ellipse(120,80,15,15);
 
-     background(bg);
+    background(bg);
+
+    fill(255,255,0,Math.abs(255-time2*255*2));
+	//ellipse(210,300,100,Math.abs(50-time*100));
+	anime(frame);
+	frame=frame+0.1;
+	if(frame >2){
+		frame = 0;
+	}
+}
+function anime(f){
+	if(f <1){
+		frame1();
+	}else if(f<2){
+		frame2();
+	}else{
+
+	}
+}
+
+
+function frame1(){
+	fill(255);
+	ellipse(10,10,100,100);
+}
+
+function frame2(){
+	fill(255,155,333);
+	ellipse(100,10,100,100);
 }
